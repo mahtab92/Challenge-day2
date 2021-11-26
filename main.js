03c06ffe7553d591b372db9699b79394c1eaedc5
Your Name <mahtab.salahshoor@gmail.com>
2021-11-25 18:34:57 -0800

    pushing changes

---
+++ main.js
@@ -0,0 +1,6 @@
+function generateDog() {
+  var image = document.createElement("img");
+  var div = document.getElementById("flex-Dog-Gen");
+  image.src = "download.jpg";
+  div.appendChild(image);
+}
